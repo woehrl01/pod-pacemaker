7bd67daca3efeaeca8017469a89d8937359eb71e
Lukas Wöhrl <lukas.woehrl@plentymarkets.com>
2024-04-20 20:56:07 +0200

    fix: handle terminated jobs as stopped (#26)

--- cmd/node-daemon/podeventhandler.go
+++ cmd/node-daemon/podeventhandler.go
@@ -38,14 +38,25 @@ func (p *PodEventHandler) OnAdd(pod *v1.Pod) {
 		return
 	}
 
-	allStarted := true
+	allStarted := true // checking if all containers are started
 	for _, containerStatus := range pod.Status.ContainerStatuses {
 		if containerStatus.Started == nil || !*containerStatus.Started {
 			allStarted = false
 			break
 		}
 	}
-	if allStarted {
+
+	allTerminated := true // checking if all containers are terminated, e.g. pod is done for jobs
+	for _, containerStatus := range pod.Status.ContainerStatuses {
+		if containerStatus.State.Terminated == nil {
+			allTerminated = false
+			break
+		}
+	}
+
+	completed := pod.Status.Phase == v1.PodSucceeded || pod.Status.Phase == v1.PodFailed
+
+	if allStarted || allTerminated || completed {
 		log.WithField("pod", slotName).Debug("Pod is fully started, releasing slot")
 		p.throttler.ReleaseSlot(p.ctx, slotName)
 	} else {
