67da94cea7c7d071575ca3ffe5739e94fde4817c
Lukas Wöhrl <lukas.woehrl@plentymarkets.com>
2024-04-20 13:20:58 +0200

    wip

--- cmd/cni-plugin/client.go
+++ cmd/cni-plugin/client.go
@@ -44,16 +44,24 @@ func WaitForSlot(slotName string, config *PluginConf) error {
 func WaitUntilConnected(ctx context.Context, socketPath string) (*grpc.ClientConn, error) {
 	server := fmt.Sprintf("unix://%s", socketPath)
 	var conn *grpc.ClientConn
+	var connErr error
 	err := wait.PollUntilContextCancel(ctx, time.Second, true, func(ctx context.Context) (bool, error) {
 		c, err := grpc.DialContext(ctx, server,
 			grpc.WithTransportCredentials(insecure.NewCredentials()),
 		)
 		if err != nil {
+			connErr = err
 			logrus.Warnf("Failed to connect to daemon: %v", err)
 			return false, nil
 		}
 		conn = c
 		return true, nil
 	})
-	return conn, err
+	if connErr != nil {
+		return nil, connErr
+	}
+	if err != nil {
+		return nil, err
+	}
+	return conn, nil
 }
